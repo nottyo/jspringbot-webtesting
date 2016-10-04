c154f3e8035d64601e57661625f1e4b9440ab9c4
Traitanit Huangsri <traitanit.hua@ascendcorp.com>
2016-10-04 14:23:51 +0700

    initial commit

---
+++ src/main/java/org/nottyo/robot/sample/webtesting/App.java
@@ -0,0 +1,13 @@
+package org.nottyo.robot.sample.webtesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/robotframework/acceptance/google-sample.robot
@@ -0,0 +1,7 @@
+*** Settings ***
+Library    JSpringBotGlobal
+
+*** Test Cases ***
+Search For Google
+    [Documentation]    Search something on Google
+    Navigate to    https://www.google.com
